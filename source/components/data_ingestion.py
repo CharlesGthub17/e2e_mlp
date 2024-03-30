f17621d9d6bfa8575cec87fa30b28bf44ee9ad5d
charles_gth <charles04007@gmail.com>
2024-03-30 17:07:46 +0530

    Data Transformation

--- source/components/data_ingestion.py
+++ source/components/data_ingestion.py
@@ -5,6 +5,9 @@ from source.logger import logging
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from source.components.data_transformation import DataTransformation
+from source.components.data_transformation import DataTransformationConfig
+
 
 @dataclass
 class DataIngestionConfig:
@@ -19,16 +22,18 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         logging.info("Entered the data ingestion method or component")
         try:
-            df= pd.read_csv(r"G:\projects\e2e_mlp\notebook\data\flipkart_mobiles.csv")
+            df= pd.read_csv(r"notebook\data\flipkart_mobiles.csv")
             logging.info("Read the dataset as dataframe")
-
+          
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
-            df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
+            
+            df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
 
             logging.info("train test split initiated")
             train_set,test_set = train_test_split(df,test_size=0.2 ,random_state=42)
 
             train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+
             test_set.to_csv(self.ingestion_config.test_data_path,index= False,header=True)
 
             logging.info("Ingestion of the data is completed")
@@ -36,10 +41,14 @@ class DataIngestion:
             return(
                 self.ingestion_config.train_data_path,
                 self.ingestion_config.test_data_path
+                
             )
         except Exception as e:
             raise CustomException(e,sys)
 
 if __name__ =="__main__":
     obj=DataIngestion()
-    obj.initiate_data_ingestion()                   
+    train_data,test_data = obj.initiate_data_ingestion()  
+
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
---
+++ source/components/data_transformation.py
@@ -0,0 +1,102 @@
+import sys
+from dataclasses import dataclass
+import numpy as np
+import pandas as pd
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.pipeline import Pipeline
+from source.exception import CustomException
+from source.logger import logging
+import os
+from source.utils import save_object 
+
+
+@dataclass
+class DataTransformationConfig:
+    preprocessor_obj_file_path=os.path.join("artifacts","preprocessor.pkl")
+
+class DataTransformation:
+    def __init__(self):
+        self.data_transformation_config=DataTransformationConfig()
+
+    def get_data_transformer_object(self,train_df,test_df):     ### for data transformation
+        try:
+            numerical_columns=["Ratings","No_of_reviews","Discount"]
+            categorical_columns= ["Name","Brand","No_of_ratings","Product_features","MSP","MRP"]
+
+            # Convert specified columns to integer data types
+            columns_to_convert = ["No_of_ratings", "MSP", "MRP"]
+            train_df[columns_to_convert] = train_df[columns_to_convert].replace(',', '', regex=True)
+            test_df[columns_to_convert] = test_df[columns_to_convert].replace(',', '', regex=True)
+
+            train_df[columns_to_convert] = train_df[columns_to_convert].astype(int)
+            test_df[columns_to_convert] = test_df[columns_to_convert].astype(int)
+            
+            num_pipeline=Pipeline(
+                steps=[
+                    ("scaler",StandardScaler())
+                ]
+            )
+        
+      
+            logging.info("Numerical Columns Standard Scaling Completed")
+
+            preprocessor = ColumnTransformer(
+                [
+                ("num_pipeline",num_pipeline,numerical_columns)    
+                ]
+            )    
+
+            return preprocessor
+        
+
+        except Exception as e:
+            raise CustomException(e,sys)    
+
+    def initiate_data_transformation(self, train_path, test_path):
+        try:
+            train_df = pd.read_csv(train_path)
+            test_df = pd.read_csv(test_path)
+
+            # Debugging: Print column names of training and testing DataFrames
+            print("Training DataFrame columns:", train_df.columns)
+            print("Testing DataFrame columns:", test_df.columns)
+
+            logging.info("Read train and test data is completed")
+            logging.info("Obtaining preprocessing object")
+
+
+            preprocessing_obj = self.get_data_transformer_object(train_df,test_df)
+
+            target_column_name = "MSP"
+            num_columns = ["Ratings", "No_of_ratings", "No_of_reviews", "MSP", "MRP", "Discount"]
+
+            input_feature_train_df = train_df.drop(columns=[target_column_name], axis=1)
+            target_feature_train_df = train_df[target_column_name]
+
+            input_feature_test_df = test_df.drop(columns=[target_column_name], axis=1)
+            target_feature_test_df = test_df[target_column_name]
+
+            logging.info(f"Applying preprocessing object on training dataframe and testing dataframe.")
+
+            input_feature_train_arr = preprocessing_obj.fit_transform(input_feature_train_df)
+            input_feature_test_arr = preprocessing_obj.transform(input_feature_test_df)
+
+            train_arr = np.c_[input_feature_train_arr, np.array(target_feature_train_df)]
+            test_arr = np.c_[input_feature_test_arr, np.array(target_feature_test_df)]
+
+            logging.info("Saved Preprocessing Object")
+
+            save_object(
+                file_path=self.data_transformation_config.preprocessor_obj_file_path,
+                obj=preprocessing_obj
+            )
+
+            return (
+                train_arr,
+                test_arr,
+                self.data_transformation_config.preprocessor_obj_file_path,
+            )
+
+        except Exception as e:
+            raise CustomException(e, sys)
