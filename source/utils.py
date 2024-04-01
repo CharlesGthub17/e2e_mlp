d205224a3fb391bd3594023b511bd27effa9258f
charles_gth <charles04007@gmail.com>
2024-04-01 16:47:15 +0530

    Hyperparameter Tuning

--- source/utils.py
+++ source/utils.py
@@ -5,6 +5,7 @@ import numpy as np
 import dill
 from source.exception import CustomException
 from sklearn.metrics import mean_squared_error,mean_absolute_error,r2_score
+from sklearn.model_selection import GridSearchCV
 
 
 def save_object(file_path,obj):
@@ -19,15 +20,23 @@ def save_object(file_path,obj):
     except Exception as e:
         raise CustomException(e,sys)        
     
-def evaluate_models(x_train,y_train,x_test,y_test,models):
+def evaluate_models(x_train,y_train,x_test,y_test,models,param):
     try:
         report={}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
-            model.fit(x_train, y_train) 
-    
-            # Make predictions
+            para=param[list(models.keys())[i]]
+
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(x_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(x_train,y_train)
+            
+            # model.fit(x_train, y_train)   # Training the model 
+
+            # Making predictions
             y_train_pred = model.predict(x_train)
             y_test_pred = model.predict(x_test)
 
@@ -45,3 +54,11 @@ def evaluate_models(x_train,y_train,x_test,y_test,models):
     
     except Exception as e:
         raise CustomException(e,sys)
+    
+# def load_object(file_path):
+#     try:
+#         with open(file_path, "rb") as file_obj:
+#             return pickle.load(file_obj)
+
+#     except Exception as e:
+#         raise CustomException(e, sys)
